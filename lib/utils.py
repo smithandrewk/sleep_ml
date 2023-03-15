c2b6bcc3caa65c66ce5edd53903add583c3cd743
Andrew <andrewsmith1025@gmail.com>
2023-03-15 19:30:59 -0400

    add notebooks

--- lib/utils.py
+++ lib/utils.py
@@ -4,11 +4,47 @@ import numpy as np
 import seaborn as sns
 import plotly.express as px
 from mne.io import read_raw_edf
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+import torch
+from torch import nn
+from torch.nn.functional import relu,one_hot
+from tqdm import tqdm
 
 def load_raw(filename):
     filepath = f'data/{filename}.edf'
     return load_raw_by_path(filepath)
+def load_raw_list(list):
+    ret = pd.DataFrame()
 
+    raw = load_raw(list[0])
+    df = load_psd(list[0])
+    eeg = raw.get_data(picks='EEG')[0]
+    X = pd.DataFrame(eeg.reshape(-1,5000))
+    y = df['label']
+
+    append = pd.concat([y,X],axis=1)
+    ret = pd.concat([ret,append])
+    for i in list[1:]:
+        raw = load_raw(i)
+        df = load_psd(i)
+        eeg = raw.get_data(picks='EEG')[0]
+        X = pd.DataFrame(eeg.reshape(-1,5000))
+        y = df['label']
+
+        append = pd.concat([y,X],axis=1)
+        ret = pd.concat([ret,append])
+
+    ret = ret.reset_index(drop=True)
+    ret = ret[ret['label'] != 'X']
+
+    y = np.array(pd.Categorical(ret.pop('label')).codes)
+    X = ret.to_numpy()
+
+    X = torch.from_numpy(X).float()
+    y = torch.from_numpy(y).reshape(-1,1).long()
+    return (X,y)
 def load_raw_by_path(path):
     raw = read_raw_edf(path,verbose=False)
     raw.rename_channels({'EEG 1':'EEG','EEG 2':'EMG'})
@@ -28,8 +64,8 @@ def load_psd(fileindex):
     df = df.rename(mapper = {'EEG 2 (Mean, 10s)':'emg'},axis=1)
     df = df.rename(mapper = {'Activity (Mean, 10s)':'activity'},axis=1)
     df = df.drop('timestamp',axis=1)
-    df = df[df['label']!='X']
-    df = df.drop('0.0',axis=1)
+    # df = df[df['label']!='X']
+    # df = df.drop('0.0',axis=1)
     return df
 
 def load_psd_list(list):
@@ -45,8 +81,18 @@ def load_all_psd():
         df = pd.concat([df,load_psd(i)])
     df = df.reset_index(drop=True)
     return df
+def leave_one_out(left_out=0):
+    df = pd.DataFrame()
+    df_left_out = pd.DataFrame()
 
-def leave_one_out():
+    for i in range(0,32):
+        if(i==left_out):
+            df_left_out = load_psd(i)
+            continue
+        df = pd.concat([df,load_psd(i)])
+    df = df.reset_index(drop=True)
+    return df,df_left_out
+def leave_random_out(left_out=0):
     nums = np.arange(32)
     np.random.shuffle(nums)
     #wlog leave nums[0] out
@@ -72,3 +118,27 @@ def remove_outliers_from_eeg(eeg):
     imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
     eeg_no_outliers = imp_mean.fit_transform(eeg)
     return eeg_no_outliers
+
+def train_test_confusion_matrices(X_train,X_test,y_train,y_test,clf,title):
+    fig,axes = plt.subplots(nrows=1,ncols=2,figsize=(7.2,4.45),dpi=500,sharex=True,sharey=True)
+    y_pred = clf.predict(X_train)
+    cm = confusion_matrix(y_train,y_pred,normalize='true')
+    sns.heatmap(ax=axes[0],data=cm,annot=True)
+    y_pred = clf.predict(X_test)
+    cm = confusion_matrix(y_test,y_pred,normalize='true')
+    sns.heatmap(ax=axes[1],data=cm,annot=True)
+    fig.supxlabel('Predicted Label')
+    fig.supylabel('True Label')
+    axes[0].set_title(f'Training Data')
+    axes[1].set_title(f'Testing Data')
+    plt.suptitle(f'{title}',fontweight='heavy')
+    plt.savefig(f'figures/{title}_train_test.jpg',dpi=200,bbox_inches='tight')
+def test_confusion_matrix(X_test,y_test,clf,title):
+    plt.figure(figsize=(5,5),dpi=500)
+    y_pred = clf.predict(X_test)
+    cm = confusion_matrix(y_test,y_pred,normalize='true')
+    sns.heatmap(data=cm,annot=True)
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.title(f'{title}',fontweight='heavy')
+    plt.savefig(f'figures/{title}_test.jpg',dpi=200,bbox_inches='tight')

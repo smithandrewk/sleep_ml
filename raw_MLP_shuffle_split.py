a928fdf07ed3bf72cfdb5768f2fdc1af3d4e0c9d
Andrew Smith <andrewsmith@sc.edu>
2023-07-03 14:14:15 -0400

    device

--- raw_MLP_shuffle_split.py
+++ raw_MLP_shuffle_split.py
@@ -67,8 +67,8 @@ print("Params: ",params)
 
 if(config['RESUME']):
     print("Resuming previous training")
-    if os.path.exists(f'{project_dir}/model.pt'):
-        model.load_state_dict(torch.load(f=f'{project_dir}/model.pt'))
+    if os.path.exists(f'{project_dir}/last_model.pt'):
+        model.load_state_dict(torch.load(f=f'{project_dir}/last_model.pt'))
     else:
         print("Model file does not exist.")
         print("Exiting because resume flag was given and model does not exist. Either remove resume flag or move model to directory.")

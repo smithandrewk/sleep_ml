3a061b383f1cf459cc1714206dacefb427b29427
Andrew <andrewsmith1025@gmail.com>
2023-07-10 12:45:25 -0400

    current

--- scripts/cv_dataset.py
+++ scripts/cv_dataset.py
@@ -1,26 +1,25 @@
 from lib.utils import *
+from lib.ekyn import *
 import json
 from tqdm import tqdm
+import sys
 
-# TODO for each fold
-foldi = 0
-train_size = .95
 window_size = 1
-data_dir = f'w{window_size}_cv_{foldi}'
-x_train_i = 0
-x_dev_i = 0
+fold = int(sys.argv[1])
+
+folds = get_leave_one_out_cv_ids_for_ekyn()
+ids,test_ids = folds[fold]
+data_dir = f'w{window_size}_cv_{fold}'
+
 normalize = False
-train_ids,test_ids = get_cross_validation_split_for_fold(foldi=foldi)
 
 os.makedirs(data_dir)
 os.makedirs(f'{data_dir}/train')
 os.makedirs(f'{data_dir}/dev')
+os.makedirs(f'{data_dir}/test')
 
 config = {
-    'TRAIN_SIZE':train_size,
-    'TRAIN_IDS':train_ids,
-    'TEST_IDS':test_ids,
-    'NORMALIZED':normalize,
+    'NORMALIZED' : normalize,
 }
 
 with open(f'{data_dir}/config.json', 'w') as f:
@@ -28,35 +27,42 @@ with open(f'{data_dir}/config.json', 'w') as f:
 
 y_train_all = torch.Tensor()
 y_dev_all = torch.Tensor()
+y_test_all = torch.Tensor()
+x_train_i = 0
+x_dev_i = 0
+x_test_i = 0
 
-for id in tqdm(train_ids):
+for id in tqdm(ids):
     for condition in ['PF','Vehicle']:
         X,y = load_eeg_label_pair(id=id,condition=condition)
-        if(normalize):
-            # center, stretch
-            X = (X - X.mean(axis=1,keepdim=True))/X.std(axis=1,keepdim=True)
-            # drop row if any element is inf
-            not_inf_idx = torch.where(~X.isinf().any(axis=1))[0]
-            X,y = X[not_inf_idx], y[not_inf_idx]
-        # train test split for each file, approximates the same for train-test-splitting the entire set
-        X_train,X_dev,y_train,y_dev = train_test_split(X,y,test_size=(1-train_size),shuffle=True,stratify=y,random_state=0)
+        if window_size != 1:
+            X = window_epoched_signal(X,window_size)
+        X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.2,shuffle=True,random_state=0)
+        X_train,X_dev,y_train,y_dev = train_test_split(X_train,y_train,test_size=.25,shuffle=True,random_state=0)
         for Xi in X_train:
             torch.save(Xi.clone(),f'{data_dir}/train/{x_train_i}.pt')
             x_train_i += 1
         for Xi in X_dev:
             torch.save(Xi.clone(),f'{data_dir}/dev/{x_dev_i}.pt')
             x_dev_i += 1
+        for Xi in X_test:
+            torch.save(Xi.clone(),f'{data_dir}/test/{x_test_i}.pt')
+            x_test_i += 1
         y_train_all = torch.cat([y_train_all,y_train])
         y_dev_all = torch.cat([y_dev_all,y_dev])
+        y_test_all = torch.cat([y_test_all,y_test])
 
 torch.save(y_train_all,f'{data_dir}/y_train.pt')
 torch.save(y_dev_all,f'{data_dir}/y_dev.pt')
+torch.save(y_test_all,f'{data_dir}/y_test.pt')
 
 from lib.datasets import Dataset2p0
 from torch.utils.data import DataLoader
 trainloader = DataLoader(Dataset2p0(dir=f'{data_dir}/train/',labels=f'{data_dir}/y_train.pt'),batch_size=1,shuffle=True)
 devloader = DataLoader(Dataset2p0(dir=f'{data_dir}/dev/',labels=f'{data_dir}/y_dev.pt'),batch_size=1,shuffle=True)
+testloader = DataLoader(Dataset2p0(dir=f'{data_dir}/test/',labels=f'{data_dir}/y_test.pt'),batch_size=1,shuffle=True)
 X,y = next(iter(trainloader))
 X,y = next(iter(devloader))
+X,y = next(iter(testloader))
 print(X.shape,y.shape)
-print(len(trainloader)+len(devloader))
+print(len(trainloader)+len(devloader)+len(testloader))

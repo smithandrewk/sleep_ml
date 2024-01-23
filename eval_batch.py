1c119be222fe7f2948edf5ac30e01b9f9ed3bd21
Andrew Smith <andrewsmith1025@gmail.com>
2024-01-23 14:36:35 -0500

    eval script

---
+++ eval_batch.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import os
+import json
+import pandas as pd
+
+df = pd.DataFrame()
+for project in os.listdir(f'projects'):
+    if not os.path.exists(f'projects/{project}/config.json'):
+        continue
+    with open(f'projects/{project}/config.json','r') as f:
+        CONFIG = json.load(f)
+    df[project] = pd.Series(CONFIG)
+df = df.T
+print(df.sort_values(by='BEST_DEV_LOSS',ascending=True)[['BEST_DEV_LOSS','BEST_DEV_F1','DEPTHI','WIDTHI']])

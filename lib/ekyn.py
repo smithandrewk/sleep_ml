c9bfae4ce6645fadd44a38239e6e60333c1235b9
Andrew <andrewsmith1025@gmail.com>
2024-02-15 16:13:03 -0500

    ekyn

--- lib/ekyn.py
+++ lib/ekyn.py
@@ -1,16 +1,15 @@
 from os import listdir
 from torch import load
-
-DATA_PATH = f'/home/andrew/.aurora'
+from lib.env import DATA_PATH
 
 def get_ekyn_ids():
-    return sorted(listdir(f'{DATA_PATH}/ekyn'))
+    return sorted(list(set([id.split('_')[0] for id in listdir(f'{DATA_PATH}/pt_ekyn')])))
 
 def load_ekyn_pt(idx,condition):
-    return load(f'/home/andrew/.aurora/pt_ekyn/{idx}_{condition}.pt')
+    return load(f'{DATA_PATH}/pt_ekyn/{idx}_{condition}.pt')
 
 def load_ekyn_pt_standardized(idx,condition):
-    return load(f'/home/andrew/.aurora/pt_ekyn_standardized/{idx}_{condition}.pt')
+    return load(f'{DATA_PATH}/pt_ekyn_standardized/{idx}_{condition}.pt')
 
 def load_ekyn_pt_robust_scaled(idx,condition):
-    return load(f'/home/andrew/.aurora/robust_scaled/{idx}_{condition}.pt')
+    return load(f'{DATA_PATH}/robust_scaled/{idx}_{condition}.pt')
